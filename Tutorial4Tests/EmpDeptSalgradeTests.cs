9d9a3e113c7964736b50df80a111b71b69152111
s31723 <s31723@pjwstk.edu.pl>
2025-04-02 19:14:43 +0200

    second test done

--- Tutorial4Tests/EmpDeptSalgradeTests.cs
+++ Tutorial4Tests/EmpDeptSalgradeTests.cs
@@ -22,7 +22,7 @@ public class EmpDeptSalgradeTests
     {
         var emps = Database.GetEmps();
 
-        List<Emp> result = null; 
+        List<Emp> result = emps.Where(e => e.DeptNo == 30).OrderByDescending(e => e.Sal).ToList(); 
 
         Assert.Equal(2, result.Count);
         Assert.True(result[0].Sal >= result[1].Sal);
@@ -36,7 +36,7 @@ public class EmpDeptSalgradeTests
         var emps = Database.GetEmps();
         var depts = Database.GetDepts();
 
-        List<Emp> result = null; 
+        List<Emp> result = emps.Where(e => e.DeptNo; 
 
         Assert.All(result, e => Assert.Equal(30, e.DeptNo));
     }
